5ba672890be4415b1df7c6445410af874ed49cc6
s-sajid <88870507+s-sajid@users.noreply.github.com>
2023-11-23 01:51:13 -0700

    Add flight and aircraft table

--- database/flight_reservation.sql
+++ database/flight_reservation.sql
@@ -13,8 +13,32 @@ create table registered_users (
 	primary key (email)
 );
 
-insert into registered_users (email, first_name, last_name, phone, address, password)
-values
-('john.doe@email.com',	'john',	'doe', '123-456-789', '123 Main Street', 'password'),
+drop table if exists aircrafts;
+create table aircrafts (
+    aircraft_id         int not null auto_increment,
+    model               varchar(50) not null,
+    primary key (aircraft_id)
+);
+
+drop table if exists flights;
+create table flights (
+    flight_number       varchar(10) not null,
+    origin              varchar(50) not null,
+    destination         varchar(50) not null,
+    departure_date      date not null,
+    aircraft_id         int not null,
+    primary key (flight_number),
+    foreign key (aircraft_id) references aircrafts(aircraft_id)
+);
+
+insert into aircrafts (model) values
+('Boeing 747'),
+('Airbus A320');
+
+insert into flights (flight_number, origin, destination, departure_date, aircraft_id) values
+('AB123', 'New York', 'Los Angeles', '2023-12-01', 1),
+('CD456', 'Chicago', 'Miami', '2023-12-02', 2);
+
+insert into registered_users (email, first_name, last_name, phone, address, password) values
 ('jack.doe@email.com',	'jack',	'doe', '123-456-789', '123 Main Street', 'password'),
 ('jill.doe@email.com',	'jill',	'doe', '123-456-789', '123 Main Street', 'password');
