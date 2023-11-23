8e6c147bbde3cbe8c98af5e485c4c9c1725d2635
s-sajid <88870507+s-sajid@users.noreply.github.com>
2023-11-23 02:37:57 -0700

    Fix formatting

--- backend/routes/flights.js
+++ backend/routes/flights.js
@@ -5,32 +5,34 @@ const router = express.Router();
 
 // Endpoint for fetching all flights
 router.get("/flights", async (req, res) => {
-    try {
-        const flights = await db.query("SELECT * FROM flights");
-        res.json(flights);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Internal Server Error" })
-    }
-})
+  try {
+    const flights = await db.query("SELECT * FROM flights");
+    res.json(flights);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 // Endpoint for fetching flights by date
 router.get("/flights/byDate", async (req, res) => {
-    try {
-      const { date } = req.query;
-  
-      if (!date) {
-        return res.status(400).json({ error: "Date parameter is required" });
-      }
-  
-      // Assuming you have a 'departure_date' column in your 'flights' table
-      const flights = await db.query("SELECT * FROM flights WHERE DATE(departure_date) = ?", [date]);
-      
-      res.json(flights);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Internal Server Error" });
+  try {
+    const { date } = req.query;
+
+    if (!date) {
+      return res.status(400).json({ error: "Date parameter is required" });
     }
-  });
-  
+
+    const flights = await db.query(
+      "SELECT * FROM flights WHERE DATE(departure_date) = ?",
+      [date]
+    );
+
+    res.json(flights);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 export default router;
