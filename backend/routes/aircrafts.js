323ff14b422d101e5b9cf83b43b8ebe4c18f0ce4
s-sajid <88870507+s-sajid@users.noreply.github.com>
2023-11-23 01:59:15 -0700

    Add get method for aircraft

---
+++ backend/routes/aircrafts.js
@@ -0,0 +1,18 @@
+import express from "express";
+import db from "../config/db.js";
+
+const router = express.Router();
+
+// Endpoint for fetching all aircrafts
+router.get("/aircrafts", async (req, res) => {
+  try {
+    // Use the Singleton instance for database queries
+    const aircrafts = await db.query("SELECT * FROM aircrafts");
+    res.json(aircrafts);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+export default router;
--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import bcrypt from "bcrypt";
 import db from "../config/db.js";
 import usersRouter from "../routes/users.js";
+import aircraftsRouter from "../routes/aircrafts.js";
 
 dotenv.config();
 
@@ -13,8 +14,9 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 
-// Use the /users base path for the usersRouter
+// Use routes
 app.use("/", usersRouter);
+app.use("/", aircraftsRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
