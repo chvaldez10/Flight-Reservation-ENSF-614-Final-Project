0e2804f75e1e61648e405352264f4acd9876ff25
s-sajid <88870507+s-sajid@users.noreply.github.com>
2023-11-22 21:34:56 -0700

    Update users schema

--- database/flight_reservation.sql
+++ database/flight_reservation.sql
@@ -4,14 +4,27 @@ use flight_reservation;
 
 drop table if exists users;
 create table users (
-	Name			varchar(25) not null,
-	Phone			char(12),
-	Address			varchar(25),
-	primary key (Name)
+	email 		varchar(50) not null,
+	firstName	varchar(25) not null,
+	last_name	varchar(25) not null,
+	phone		char(12),
+	address		varchar(25),
+	primary key (email)
 );
 
-insert into users (Name, Phone, Address)
+insert into users (email, firstName, last_name, phone, address)
 values
-('A',	'1',	'5'),
-('B',	'2',	'5'),
-('C.',	'3',	'5');
+('john.doe@email.com',	'john',	'doe', '123-456-789', '123 Main Street'),
+('jack.doe@email.com',	'jack',	'doe', '123-456-789', '123 Main Street'),
+('jill.doe@email.com',	'jill',	'doe', '123-456-789', '123 Main Street');
+
+drop table if exists registered_users;
+create table registered_users (
+	email 		varchar(50) not null,
+	firstName	varchar(25) not null,
+	last_name	varchar(25) not null,
+	phone		char(12),
+	address		varchar(25),
+	password	varchar(50) not null,
+	primary key (email)
+);
