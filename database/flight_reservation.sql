0e6e39c8c10dcd2ca78a3372503ee473856dfe20
s-sajid <88870507+s-sajid@users.noreply.github.com>
2023-11-23 00:48:07 -0700

    Add registered users table

--- database/flight_reservation.sql
+++ database/flight_reservation.sql
@@ -2,29 +2,19 @@ drop database if exists flight_reservation;
 create database flight_reservation;
 use flight_reservation;
 
-drop table if exists users;
-create table users (
-	email 		varchar(50) not null,
-	firstName	varchar(25) not null,
-	last_name	varchar(25) not null,
-	phone		char(12),
-	address		varchar(25),
-	primary key (email)
-);
-
-insert into users (email, firstName, last_name, phone, address)
-values
-('john.doe@email.com',	'john',	'doe', '123-456-789', '123 Main Street'),
-('jack.doe@email.com',	'jack',	'doe', '123-456-789', '123 Main Street'),
-('jill.doe@email.com',	'jill',	'doe', '123-456-789', '123 Main Street');
-
 drop table if exists registered_users;
 create table registered_users (
 	email 		varchar(50) not null,
-	firstName	varchar(25) not null,
+	first_name	varchar(25) not null,
 	last_name	varchar(25) not null,
-	phone		char(12),
-	address		varchar(25),
-	password	varchar(50) not null,
+	phone		varchar(20),
+	address		varchar(225),
+	password	varchar(225) not null,
 	primary key (email)
 );
+
+insert into registered_users (email, first_name, last_name, phone, address, password)
+values
+('john.doe@email.com',	'john',	'doe', '123-456-789', '123 Main Street', 'password'),
+('jack.doe@email.com',	'jack',	'doe', '123-456-789', '123 Main Street', 'password'),
+('jill.doe@email.com',	'jill',	'doe', '123-456-789', '123 Main Street', 'password');
