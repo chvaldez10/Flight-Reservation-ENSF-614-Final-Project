617ccf868cde612dd15ebd364364450b1c53dd31
chvaldez10 <christianvaldez021@gmail.com>
2023-11-26 09:34:13 -0700

    sample flight list

--- frontend/src/pages/list/List.jsx
+++ frontend/src/pages/list/List.jsx
@@ -1,6 +1,11 @@
 import React from "react";
-import "./list.css";
+import "./list.css"; // Make sure this file exists and is correctly linked
 import { Card, CardContent, Typography, Grid } from "@mui/material";
+import FlightTakeoffIcon from "@mui/icons-material/FlightTakeoff";
+import EventSeatIcon from "@mui/icons-material/EventSeat";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import DateRangeIcon from "@mui/icons-material/DateRange";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
 
 // Sample data for testing
 const flightData = [
@@ -10,10 +15,20 @@ const flightData = [
     departureDate: "2023-11-26",
     departureTime: "08:20",
     arrivalTime: "09:21",
-    economyPrice: 192,
-    businessPrice: "-", // Assuming "-" means not available
+    economyPrice: 69,
+    comfortPrice: 169,
+    businessPrice: "-",
   },
-  // ... other flight objects
+  {
+    origin: "Calgary",
+    destination: "Edmonton",
+    departureDate: "2023-11-26",
+    departureTime: "08:50",
+    arrivalTime: "09:51",
+    economyPrice: 69,
+    comfortPrice: 169,
+    businessPrice: "-",
+  },
 ];
 
 const List = () => {
@@ -25,25 +40,39 @@ const List = () => {
       {flightData.map((flight, index) => (
         <Card key={index} className="flight-item">
           <CardContent>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={3} className="flight-info">
-                <Typography>{flight.departureTime}</Typography>
-                <Typography>Non-stop - 1hr01m</Typography>
+            <Grid
+              container
+              spacing={2}
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Grid item xs={2} className="flight-info">
+                <FlightTakeoffIcon className="icon" />
                 <Typography>
                   {flight.origin} - {flight.destination}
                 </Typography>
               </Grid>
-              <Grid item xs={6} sm={3} className="flight-info">
-                <Typography>Departure: {flight.departureDate}</Typography>
-                <Typography>Arrival Time: {flight.arrivalTime}</Typography>
+              <Grid item xs={2} className="flight-info">
+                <DateRangeIcon className="icon" />
+                <Typography>{flight.departureDate}</Typography>
               </Grid>
-              <Grid item xs={6} sm={3} className="price-info">
-                <Typography>Economy</Typography>
-                <Typography>${flight.economyPrice}</Typography>
+              <Grid item xs={2} className="flight-info">
+                <AccessTimeIcon className="icon" />
+                <Typography>
+                  {flight.departureTime} - {flight.arrivalTime}
+                </Typography>
               </Grid>
-              <Grid item xs={6} sm={3} className="price-info">
-                <Typography>Business Class</Typography>
-                <Typography>{flight.businessPrice}</Typography>
+              <Grid item xs={2} className="price-info">
+                <EventSeatIcon className="icon" />
+                <Typography>Economy ${flight.economyPrice}</Typography>
+              </Grid>
+              <Grid item xs={2} className="price-info">
+                <EventSeatIcon className="icon" />
+                <Typography>Comfort ${flight.comfortPrice}</Typography>
+              </Grid>
+              <Grid item xs={2} className="price-info">
+                <AttachMoneyIcon className="icon" />
+                <Typography>Business {flight.businessPrice}</Typography>
               </Grid>
             </Grid>
           </CardContent>
