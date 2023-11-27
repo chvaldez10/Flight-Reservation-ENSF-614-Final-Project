89fc2f7d88c3bfc0901fb159473e77f53dc6b3fa
chvaldez10 <christianvaldez021@gmail.com>
2023-11-26 22:03:23 -0700

    start seatmap component

--- frontend/src/pages/checkout/Checkout.jsx
+++ frontend/src/pages/checkout/Checkout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./checkout.css";
 import Payment from "../../components/payment/Payment";
+import Seatmap from "../../components/seatmap/Seatmap";
 
 const Checkout = () => {
   return (
     <div>
+      <Seatmap />
       <Payment />
     </div>
   );
