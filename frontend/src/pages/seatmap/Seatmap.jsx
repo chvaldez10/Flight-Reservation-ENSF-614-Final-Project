50a5bbcce6225438ef6b57293d41f7a962a14bbd
chvaldez10 <christianvaldez021@gmail.com>
2023-11-27 08:59:09 -0700

    moved seatmap components

--- frontend/src/pages/seatmap/Seatmap.jsx
+++ frontend/src/pages/seatmap/Seatmap.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Box, Typography, Grid, useTheme, useMediaQuery } from "@mui/material";
-import SeatRow from "./SeatRow";
+import SeatRow from "../../components/seatmap/SeatRow";
 
 const Seatmap = () => {
   const theme = useTheme();
