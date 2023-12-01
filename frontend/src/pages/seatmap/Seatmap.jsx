4ee263f682e55f90e7b7227c9be5b6610c7df425
chvaldez10 <christianvaldez021@gmail.com>
2023-12-01 11:52:41 -0700

    update

--- frontend/src/pages/seatmap/Seatmap.jsx
+++ frontend/src/pages/seatmap/Seatmap.jsx
@@ -1,6 +1,14 @@
 import React, { useState } from "react";
-import { Box, Typography, Grid, useTheme, useMediaQuery } from "@mui/material";
+import {
+  Button,
+  Box,
+  Typography,
+  Grid,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
 import SeatRow from "../../components/seatmap/SeatRow";
+import Navbar from "../../components/navbar/NavbarComponent";
 import { useBookingDetails } from "../../context/BookingDetailsContext";
 
 /**
@@ -40,29 +48,39 @@ const Seatmap = () => {
   };
 
   return (
-    <Box
-      sx={{
-        textAlign: "center",
-        width: "100%",
-        overflowX: "auto",
-        margin: "0 auto",
-        padding: "1rem",
-      }}
-    >
-      <Typography variant="h4" gutterBottom component="div">
-        Seatmap
-      </Typography>
-      <Grid container direction={isSmallScreen ? "column" : "row"} spacing={2}>
-        {Array.from({ length: numRows }, (_, index) => (
-          <SeatRow
-            key={index}
-            rowNum={index + 1}
-            onSelect={handleSeatSelect}
-            selectedSeat={selectedSeat}
-          />
-        ))}
-      </Grid>
-    </Box>
+    <div>
+      <Navbar />
+      <Box
+        sx={{
+          textAlign: "center",
+          width: "100%",
+          overflowX: "auto",
+          margin: "0 auto",
+          padding: "1rem",
+        }}
+      >
+        <Typography variant="h4" gutterBottom component="div">
+          Seatmap
+        </Typography>
+        <Grid
+          container
+          direction={isSmallScreen ? "column" : "row"}
+          spacing={2}
+        >
+          {Array.from({ length: numRows }, (_, index) => (
+            <SeatRow
+              key={index}
+              rowNum={index + 1}
+              onSelect={handleSeatSelect}
+              selectedSeat={selectedSeat}
+            />
+          ))}
+        </Grid>
+        <Button variant="contained" color="primary" fullWidth>
+          Continue
+        </Button>
+      </Box>
+    </div>
   );
 };
 
