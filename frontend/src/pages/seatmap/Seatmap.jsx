76a38dea8f31e541eee0aa7ee166ea4a1bca2c61
chvaldez10 <christianvaldez021@gmail.com>
2023-11-30 23:15:06 -0700

    refactored seatmap

--- frontend/src/pages/seatmap/Seatmap.jsx
+++ frontend/src/pages/seatmap/Seatmap.jsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Typography, Grid, useTheme, useMediaQuery } from "@mui/material";
 import SeatRow from "../../components/seatmap/SeatRow";
 
 const Seatmap = () => {
   const theme = useTheme();
   const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
-
-  const columns = ["A", "B", "C", "D"];
-  const numRows = 20;
+  const numRows = 10;
+  const [selectedSeat, setSelectedSeat] = useState(null);
 
   const handleSeatSelect = (seat) => {
-    console.log(`Selected seat: ${seat}`);
+    if (seat === selectedSeat) {
+      setSelectedSeat(null); // Deselect if the same seat is clicked
+    } else {
+      setSelectedSeat(seat); // Select a new seat
+    }
+    console.log(`Select seat: ${seat}`);
   };
 
   return (
@@ -31,8 +35,8 @@ const Seatmap = () => {
           <SeatRow
             key={index}
             rowNum={index + 1}
-            columns={columns}
             onSelect={handleSeatSelect}
+            selectedSeat={selectedSeat}
           />
         ))}
       </Grid>
