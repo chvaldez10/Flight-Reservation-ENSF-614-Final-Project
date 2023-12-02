69b551811904ea6ac5d0ca7a3a5d0160542e04e4
chvaldez10 <christianvaldez021@gmail.com>
2023-12-01 18:49:41 -0700

    refactored passenger end point

--- backend/routes/passenger.js
+++ backend/routes/passenger.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get("/passenger/:flightID", async (req, res) => {
   try {
     const flightID = req.params.flightID;
-  
+
     if (!flightID) {
       return res.status(400).json({ error: "Flight ID parameter is required" });
     }
@@ -21,19 +21,41 @@ router.get("/passenger/:flightID", async (req, res) => {
 });
 
 // Endpoint for writing to passenger table
-router.post("/passenger", async (req, res) => { 
+router.post("/passenger", async (req, res) => {
   try {
-    const { BookingID, LName, FName, SeatLetter, SeatNum, FlightID, Email} = req.body;
+    const { BookingID, LName, FName, SeatLetter, SeatNum, FlightID, Email } =
+      req.body;
 
-    if (!BookingID || !LName || !FName || !SeatLetter || !SeatNum || FlightID || Email) {
+    if (
+      !BookingID ||
+      !LName ||
+      !FName ||
+      !SeatLetter ||
+      !SeatNum ||
+      !FlightID ||
+      !Email
+    ) {
       return res.status(400).json({ error: "Required parameters are missing" });
     }
-    const query = "INSERT INTO Passengers (BookingID, LName, FName, SeatLetter, SeatNum, FlightID, Email) VALUES (?, ?, ?, ?, ?, ?, ?)";
-    await db.query(query, [BookingID, LName, FName, SeatLetter, SeatNum, FlightID, Email]);
+
+    const query =
+      "INSERT INTO Passengers (BookingID, LName, FName, SeatLetter, SeatNum, FlightID, Email) VALUES (?, ?, ?, ?, ?, ?, ?)";
+    await db.query(query, [
+      BookingID,
+      LName,
+      FName,
+      SeatLetter,
+      SeatNum,
+      FlightID,
+      Email,
+    ]);
+    res.status(200).json({ message: "Passenger added successfully" });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "Internal Server Error" });
+    res
+      .status(500)
+      .json({ error: "Internal Server Error", message: error.message });
   }
 });
-  
+
 export default router;
--- frontend/src/context/BookingDetailsContext.js
+++ frontend/src/context/BookingDetailsContext.js
@@ -13,7 +13,6 @@ export const BookingDetailsProvider = ({ children }) => {
     hasInsurance: false,
     firstName: "",
     lastName: "",
-    phoneNumber: "",
     nameOnCard: "",
     cardNumber: "",
     expirationDate: "",
