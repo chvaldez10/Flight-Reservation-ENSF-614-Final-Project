7136514657555e1db90994e1b44697bf0032741e
s-sajid <88870507+s-sajid@users.noreply.github.com>
2023-12-02 00:38:13 -0700

    Restyle the staff page

--- frontend/src/pages/staff/Staff.jsx
+++ frontend/src/pages/staff/Staff.jsx
@@ -1,27 +1,51 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+
 import Navbar from "../../components/navbar/NavbarComponent.jsx";
-import Footer from "../../components/footer/Footer.jsx";
 
 const Staff = () => {
   const [passengers, setPassengers] = useState([]);
-  const [selectedFlight, setSelectedFlight] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
+  const [selectedFlight, setSelectedFlight] = useState("");
+  const [flights, setFlights] = useState([]);
   const [error, setError] = useState(null);
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarSeverity, setSnackbarSeverity] = useState("info");
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+
+  useEffect(() => {
+    const fetchFlights = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/api/flights");
+        setFlights(response.data);
+      } catch (error) {
+        console.error("Error fetching flights:", error);
+      }
+    };
+
+    fetchFlights();
+  }, []);
 
   const handleGetPassengers = async () => {
     try {
-      setIsLoading(true);
-      const response = await axios.get(`http://localhost:3001/api/passenger/${selectedFlight}`);
+      const response = await axios.get(
+        `http://localhost:3001/api/passenger/${selectedFlight}`
+      );
       setPassengers(response.data);
+
+      if (response.data.length === 0) {
+        setSnackbarMessage("No passengers on this flight");
+        setSnackbarSeverity("info");
+        setSnackbarOpen(true);
+      }
     } catch (error) {
-      console.error('Error fetching passengers:', error);
-      setError('Error fetching passengers');
+      console.error("Error fetching passengers:", error);
+      setError("Error fetching passengers");
+      setSnackbarOpen(true);
     } finally {
-      setIsLoading(false);
     }
   };
-
 
   return (
     <div>
@@ -30,24 +54,31 @@ const Staff = () => {
       {/* Select Flight */}
       <label className="label">
         Select Flight:
-        <input
-          type="text"
+        <select
           value={selectedFlight}
           onChange={(e) => setSelectedFlight(e.target.value)}
           className="input"
-        />
+        >
+          <option value="" disabled>
+            Select a flight
+          </option>
+          {flights.map((flight) => (
+            <option key={flight.FlightID} value={flight.FlightID}>
+              {flight.FlightID}
+            </option>
+          ))}
+        </select>
       </label>
+
       <button className="button" onClick={handleGetPassengers}>
         Get Passengers
       </button>
-      {isLoading && <p>Loading passengers...</p>}
-      {error && <div className="error-message">{error}</div>}
       {/* Passengers Table */}
       <table className="table">
         <thead>
           <tr>
-            <th className="th">Last Name</th>
             <th className="th">First Name</th>
+            <th className="th">Last Name</th>
             <th className="th">Seat</th>
             <th className="th">Email</th>
           </tr>
@@ -55,15 +86,29 @@ const Staff = () => {
         <tbody>
           {passengers.map((passenger, index) => (
             <tr key={index}>
-              <td className="td">{passenger.LName}</td>
               <td className="td">{passenger.FName}</td>
+              <td className="td">{passenger.LName}</td>
               <td className="td">{`${passenger.SeatLetter}${passenger.SeatNum}`}</td>
               <td className="td">{passenger.Email}</td>
             </tr>
           ))}
         </tbody>
       </table>
-      <Footer />
+
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={6000}
+        onClose={() => setSnackbarOpen(false)}
+      >
+        <MuiAlert
+          elevation={6}
+          variant="filled"
+          onClose={() => setSnackbarOpen(false)}
+          severity={snackbarSeverity}
+        >
+          {snackbarMessage}
+        </MuiAlert>
+      </Snackbar>
     </div>
   );
 };
