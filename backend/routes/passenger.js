7e927344a7769f343fc77e5991dd6ac7c61fad42
s-sajid <88870507+s-sajid@users.noreply.github.com>
2023-12-03 17:32:08 -0700

    Add update to booking and payment transaction table for cancellations

--- backend/routes/passenger.js
+++ backend/routes/passenger.js
@@ -77,17 +77,36 @@ router.delete("/passenger/:bookingID/:email", async (req, res) => {
 
     const { SeatLetter, SeatNum, FlightID } = seatResult[0];
 
+    // Update the PaymentTransaction table to set the BookingID to null
+    const updatePaymentTransactionQuery =
+      "UPDATE PaymentTransaction SET BookingID = null WHERE BookingID = ?";
+    await db.query(updatePaymentTransactionQuery, [bookingID]);
+
     // Delete the passenger record from the Passengers table
-    const deleteQuery =
+    const deletePassengerQuery =
       "DELETE FROM Passengers WHERE BookingID = ? AND Email = ?";
-    await db.query(deleteQuery, [bookingID, email]);
+    await db.query(deletePassengerQuery, [bookingID, email]);
+
+    // Check if there are no more passengers for the booking
+    const remainingPassengersQuery =
+      "SELECT COUNT(*) AS PassengerCount FROM Passengers WHERE BookingID = ?";
+    const remainingPassengersResult = await db.query(
+      remainingPassengersQuery,
+      [bookingID]
+    );
+
+    if (remainingPassengersResult[0].PassengerCount === 0) {
+      // If no more passengers, delete the booking from the Bookings table
+      const deleteBookingQuery = "DELETE FROM Booking WHERE BookingID = ?";
+      await db.query(deleteBookingQuery, [bookingID]);
+    }
 
     // Update the seat availability in the SeatMap table
     const updateSeatQuery =
       "UPDATE SeatMap SET Availability = TRUE WHERE SeatLetter = ? AND SeatNum = ? AND FlightID = ?";
     await db.query(updateSeatQuery, [SeatLetter, SeatNum, FlightID]);
 
-    res.json({ message: "Passenger deleted successfully" });
+    res.json({ message: "Passenger and booking deleted successfully" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
